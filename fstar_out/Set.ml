7ee3c6262444bc417f9bdc31952de7a91ad2a2a2
remy <remyciterin@gmail.com>
2021-11-29 15:09:58 +0100

    add of differents functions in module Set

--- fstar_out/Set.ml
+++ fstar_out/Set.ml
@@ -8,3 +8,33 @@ let add : 'a . 'a comparaison -> 'a -> ('a, unit) set -> ('a, unit) set =
   fun f -> fun x -> fun input -> AVL.add f x input
 let remove : 'a . 'a comparaison -> 'a -> ('a, unit) set -> ('a, unit) set =
   fun f -> fun x -> fun input -> AVL.remove f x input
+let find_min :
+  'a . 'a comparaison -> ('a, unit) set -> 'a FStar_Pervasives_Native.option
+  =
+  fun f ->
+    fun input ->
+      match input with
+      | BinTree.Leaf -> FStar_Pervasives_Native.None
+      | BinTree.Node (l, k, uu___, r) ->
+          FStar_Pervasives_Native.Some (AVL.find_min f input)
+let find_max :
+  'a . 'a comparaison -> ('a, unit) set -> 'a FStar_Pervasives_Native.option
+  =
+  fun f ->
+    fun input ->
+      match input with
+      | BinTree.Leaf -> FStar_Pervasives_Native.None
+      | BinTree.Node (l, k, uu___, r) ->
+          FStar_Pervasives_Native.Some (AVL.find_max f input)
+let remove_min : 'a . 'a comparaison -> ('a, unit) set -> ('a, unit) set =
+  fun f ->
+    fun input ->
+      match input with
+      | BinTree.Leaf -> BinTree.Leaf
+      | BinTree.Node (l, k, uu___, r) -> AVL.remove_min f input
+let remove_max : 'a . 'a comparaison -> ('a, unit) set -> ('a, unit) set =
+  fun f ->
+    fun input ->
+      match input with
+      | BinTree.Leaf -> BinTree.Leaf
+      | BinTree.Node (l, k, uu___, r) -> AVL.remove_max f input
